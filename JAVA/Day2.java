ba2e61e89a9bd7191cefaa6f79f6ddca1c4decd4
Aharva Karale <atharvakarale2604@gmail.com>
2024-10-02 22:57:08 +0530

    Add files via upload

---
+++ JAVA/Day2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Day2{
+public static void main (String args[]){
+Scanner scanner =new Scanner (System.in);
+System.out.println("Enter any number ");
+int n = scanner.nextInt();
+if(n%2==0){
+
+System.out.println("Number is Even");
+                  }
+else{
+System.out.println("Number is Odd");
+       }
+   }
+}
