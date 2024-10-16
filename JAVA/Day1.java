d856fd4ebea791ae44a85a6bd75ee5957912a611
Aharva Karale <atharvakarale2604@gmail.com>
2024-10-16 21:22:13 +0530

    Update Day1.java

--- JAVA/Day1.java
+++ JAVA/Day1.java
@@ -1,3 +1,5 @@
+//Addition of two Number
+
 class Day1 {
 
   public static void main(String[] args) {
@@ -5,7 +7,7 @@ class Day1 {
     int first = 10;
     int second = 20;
 
-    // add two numbers
+    
     int sum = first + second;
     System.out.println(first + " + " + second + " = "  + sum);
   }
