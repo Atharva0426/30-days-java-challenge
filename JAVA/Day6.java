ab8514a0e79e6a441d7e6142f1b9f5c69b776e39
Aharva Karale <atharvakarale2604@gmail.com>
2024-10-17 23:30:03 +0530

    Add files via upload

---
+++ JAVA/Day6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Day6{
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your Name ");
+        String name =s.nextLine();
+    System.out.println("Enter your Age ");
+       int age =s.nextInt();
+       System.out.println("Name "+name);
+       System.out.println("Age "+age);
+        
+    }
+}
